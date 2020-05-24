510a1dbf5ad4bc75c1c6fa6579df4c68a192919d
William Moreira da Silva <william.moreirasilva@hotmail.com>
2020-05-23 21:18:28 -0300

    Basic, add login and routes dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import routerMapper from './shared/libs/RouterMapper';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+import routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Switch>
+          {routerMapper(routes)}
+        </Switch>
+      </Router>
+
     </div>
   );
 }
+
 
 export default App;
